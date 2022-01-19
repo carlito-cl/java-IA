2b5eb18dfede1146048f8108c0c50e563e9e41b8
carlito-cl <70000266+carlito-cl@users.noreply.github.com>
2022-01-19 10:33:08 +0800

    fixed classes problem

--- src/personController.java
+++ src/personController.java
@@ -4,6 +4,18 @@ public class personController {
 
     public static void main(String[] args) {
         Person Alex = new Person("Alex Leroux", "alexlerouxmccarroll@gmail.com", true);
+        Person Charles = new Person("Charles Leroux", "charlesleroux2004@gmail.com",true);
+        Person Elodie = new Person("Elodie Palasse-Leroux", "epalasse@gmail.com", false);
+        Person Bernard = new Person("Bernard Leroux", null, true);
+        Person Anne = new Person("Anne Leroux-Mcarroll", null, false);
+        Person Stephen = new Person("Stephen Leroux", null, true);
+        Person Agnès = new Person("Agnès Leroux", null, false);
+        Person Kathy = new Person("Kathy Leroux", null, false);
+        Person Jeremy = new Person("Jeremy Fournier", null, true);
+        Person Maxime = new Person("Maxime Leroux", null, true);
+        Person PierreAlex = new Person("Pierre-Alex", null, true);
+        Person Stanislas = new Person("Stanislas Leroux", null, true);
+        Person Adrien = new Person("Adrien Fournier-Leroux", null, true);
 
     }
 }
